bc60ea50ad7e11d946ce54e8fe55f9e811dcfe3b
Abdul Muhaymen Ali <abdulmuhaymenali@gmail.com>
2026-06-07 15:06:02 +0800

    Add package structure and placeholder bulk_rename function

---
+++ src/bulk_file_renamer/renamer.py
@@ -0,0 +1,8 @@
+# Goal: Function that takes a folder path and renames all files sequentailly.
+# Step 1: Accept a folder path and print it.
+
+from pathlib import Path
+
+def bulk_rename(folder_path: Path) -> None:
+    """Placeholder: print the given folder path. """
+    print(f"Recevied folder: {folder_path}")
